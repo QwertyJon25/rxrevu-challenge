34d9d36ed1e0a059a3e6e511ac459bfcf4ba07a2
Jon Courtier <jonaqwerty@yahoo.com>
2022-03-10 20:19:59 -0500

    adjusting logic further

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ export default function App() {
     } else {
       setShown("X");
     }
-    getWinningState(newGame);
+    getWinningState(newGrid);
   };
 
-  const getWinningState = (newGame) => {
+  const getWinningState = (newGrid) => {
     const winningStates = [
       [0, 1, 2],
       [3, 4, 5],
@@ -33,40 +33,40 @@ export default function App() {
 
     winningStates.forEach((s) => {
       if (
-        newGame[s[0]] !== "" &&
-        newGame[s[0]] === newGame[s[1]] &&
-        newGame[s[1]] === newGame[s[2]]
+        newGrid[s[0]] !== "" &&
+        newGrid[s[0]] === newGrid[s[1]] &&
+        newGrid[s[1]] === newGrid[s[2]]
       ) {
-        setWin(newGame[s[0]]);
+        setAnswer(newGrid[s[0]]);
       }
     });
   };
 
   return (
     <div className="App">
-      <h1>Tic-tac-toe</h1>
+      <h1>Occupied Tiles UI</h1>
       <div>
         <span>
-          <button onClick={() => clickHandler(0)}>{game[0]}</button>
-          <button onClick={() => clickHandler(1)}>{game[1]}</button>
-          <button onClick={() => clickHandler(2)}>{game[2]}</button>
+          <button onClick={() => clickHandler(0)}>{grid[0]}</button>
+          <button onClick={() => clickHandler(1)}>{grid[1]}</button>
+          <button onClick={() => clickHandler(2)}>{grid[2]}</button>
         </span>
       </div>
       <div>
         <span>
-          <button onClick={() => clickHandler(3)}>{game[3]}</button>
-          <button onClick={() => clickHandler(4)}>{game[4]}</button>
-          <button onClick={() => clickHandler(5)}>{game[5]}</button>
+          <button onClick={() => clickHandler(3)}>{grid[3]}</button>
+          <button onClick={() => clickHandler(4)}>{grid[4]}</button>
+          <button onClick={() => clickHandler(5)}>{grid[5]}</button>
         </span>
       </div>
       <div>
         <span>
-          <button onClick={() => clickHandler(6)}>{game[6]}</button>
-          <button onClick={() => clickHandler(7)}>{game[7]}</button>
-          <button onClick={() => clickHandler(8)}>{game[8]}</button>
+          <button onClick={() => clickHandler(6)}>{grid[6]}</button>
+          <button onClick={() => clickHandler(7)}>{grid[7]}</button>
+          <button onClick={() => clickHandler(8)}>{grid[8]}</button>
         </span>
       </div>
-      Winner: {win}
+      Answer: {answer}
     </div>
   );
 }
